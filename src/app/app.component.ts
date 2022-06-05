f35fbb7527311e78d296c4e8cf080dede9a66448
ronak1644 <ronak@LAPTOP-BCR56133.(none)>
2022-06-05 15:25:08 -0500

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ronak';
+  a:string='hello' ;
+  b : any[] = [{"c":"211"}, {"c":"21"}, {"c":"323"} ];
+  
+divit : any[] = [ {
+  "ID": "1",
+  "url": 'C:\Users\ronak\ronak\src\app\r.png'
+},
+
+{
+  "ID": "2",
+  "url": 'C:\Users\ronak\ronak\src\app\s.png'
+} ];
+}
+
+
+ 
